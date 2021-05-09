9805fa3eec5970a7669fea25b58ff760b74417f5
Malte <malte@holledig.dk>
2021-05-09 21:37:39 +0200

    second commit

---
+++ cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4734bd1c3178b888c89f8d298e78acc9'),
+    },
+  },
+});
---
+++ cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '45ce69fa-4e28-4387-b99e-aae33b0eb169'
+};
